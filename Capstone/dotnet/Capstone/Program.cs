20c5554315a6bbb7d307d8d696802d774d44c95b
brian davis <thebrianjdavis@gmail.com>
2021-02-04 10:52:58 -0500

    initial file generation

--- Capstone/dotnet/Capstone/Program.cs
+++ Capstone/dotnet/Capstone/Program.cs
@@ -6,7 +6,41 @@ namespace Capstone
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// <summary>
+            /// Create variable and while loop to keep program running
+            /// </summary>
+            
+            bool appIsRunning = true;
+            while (appIsRunning)
+            {
+                /// <summary>
+                /// Call InventoryLoad method to read current inventory from file
+                /// 
+                /// This will be a transparent process that is not displayed to user
+                /// until called on by the ViewInventory method
+                /// </summary>
+                
+                // InventoryClass.InventoryLoad();
+
+                /// <summary>
+                /// Call MainMenu and set logic to call next information
+                /// </summary>
+                
+                TextPrompts.MainMenu();
+                string menuSelection = Console.ReadLine();
+                Console.Clear();
+                if(menuSelection == "3")
+                {
+                    appIsRunning = false;
+                    break;
+                }
+                else
+                {
+                    TextPrompts.MainMenu();
+                    menuSelection = Console.ReadLine();
+                    Console.Clear();
+                }
+            }
         }
     }
 }
---
+++ Capstone/dotnet/Capstone/TextPrompts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    class TextPrompts
+    {
+        public static void MainMenu()
+        {
+            /// <summary>
+            /// MainMenu display to console 
+            /// </summary>
+            Console.WriteLine("TE Team Zero Vending Machine");
+            Console.WriteLine("(1) Display Vending Machine Items");
+            Console.WriteLine("(2) Purchase");
+            Console.WriteLine("(3) Exit");
+            Console.WriteLine("\nPlease make your selection and press RETURN");
+        }
+    }
+}
